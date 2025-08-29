20d9b2e2c04717654793cdb68d42ebf339646f8e
ABDULKAREEM-010 <kareemmohammedabdul616@gmail.com>
2025-08-29 19:42:08 +0530

    Fixed Databased

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase-admin";
-import { cert, getApps } from "firebase-admin/app";
+// import { initializeApp } from "firebase-admin";
+import { initializeApp,cert, getApps } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
